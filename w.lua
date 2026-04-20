c0508f517ca824ed93f92eab4bffaffe65063185
o0oooo-o0 <jznizr@gmail.com>
2026-04-19 19:26:01 -0500

    Add initial data structure in w.lua

---
+++ w.lua
@@ -0,0 +1,11 @@
+{
+    "ea": [
+        "llllllaaaaaarrrrpp",
+        "VukioStar",
+        "dh6u",
+        "MRHerobrine2003",
+        "Reapvltalized",
+        "vukiowashere",
+        "therizzman7361"
+    ]
+}
